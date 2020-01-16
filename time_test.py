d66637e1a403e76303ab0cdf3426bf3247d5b619
Katie Dagon <katie.dagon@gmail.com>
2020-01-15 17:46:50 -0700

    Work on manuscript figures

--- time_test.py
+++ time_test.py
@@ -13,10 +13,12 @@ def mean_sq_err(y_true,y_pred):
 
 # import saved model
 from keras.models import load_model
-model = load_model('NN_finalize_multi-dim.h5', custom_objects={'mean_sq_err':mean_sq_err})
+#model = load_model('NN_finalize_multi-dim.h5', custom_objects={'mean_sq_err':mean_sq_err})
+model = load_model('emulators/NN_GPP_finalize_multi-dim.h5',
+        custom_objects={'mean_sq_err':mean_sq_err})
 
 # make predictions
-inputdata_inflate = np.load(file="lhc_1000.npy")
+inputdata_inflate = np.load(file="lhc_1000.npy", allow_pickle=True)
 model_preds_inflate = model.predict(inputdata_inflate)
 pred_time = time.time()
 
